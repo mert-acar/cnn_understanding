a394fefbf179e08fa54d8a14b83069a87fa1daac
Mert Acar <mert25acar@gmail.com>
2024-09-22 16:57:48 +0300

    lol

--- src/model.py
+++ src/model.py
@@ -8,21 +8,17 @@ class ConvNet(nn.Module):
     self.features = nn.Sequential(
       nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
       nn.ReLU(inplace=True),
-      nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
 
+      nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
       nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
       nn.ReLU(inplace=True),
-      nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
 
+      nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
       nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
       nn.ReLU(inplace=True),
-      nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-      nn.ReLU(inplace=True),
-      nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
 
+      nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
       nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
-      nn.ReLU(inplace=True),
-      nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
       nn.ReLU(inplace=True),
     )
     self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1,1))
