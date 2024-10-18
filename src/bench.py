84d425c7cd3903cc70dbf75d509f2d0fd9f9bd84
macar <mert25acar@gmail.com>
2024-10-18 13:04:49 +0300

    I have no idea what I'm doing here.

--- src/bench.py
+++ src/bench.py
@@ -1,29 +1,30 @@
 import os
+import pickle as p
 import numpy as np
 from pprint import pprint
+from scipy.spatial.distance import cdist
 from sklearn import cluster
 from scipy.io import loadmat
-from sklearn import manifold
-import matplotlib.pyplot as plt
 from cluster import parameter_search
 from dim_reduction import svd_reduction
 from cluster import select_random_samples
 
 if __name__ == "__main__":
   exp_dir = "../logs/customnet_run2/"
-  out_dir = os.path.join(exp_dir, "figures")
-  os.makedirs(out_dir, exist_ok=True)
+  # out_dir = os.path.join(exp_dir, "figures")
+  # os.makedirs(out_dir, exist_ok=True)
 
   labels = loadmat("../data/labels.mat")["labels"][0]
 
   epoch = 33
-  mds = manifold.MDS(n_components=2, metric=False, max_iter=1000, eps=1e-8, n_jobs=-1)
+  out = {}
+  # mds = manifold.MDS(n_components=2, metric=False, max_iter=1000, eps=1e-8, n_jobs=-1)
   for i in range(0, 9, 2):
     vars = [f"features.{i}_input", f"features.{i}_output"]
-    data = loadmat(
-      os.path.join(exp_dir, "activations", f"patches_epoch_{epoch}.mat"), variable_names=vars
-    )
     for var in vars:
+      data = loadmat(
+        os.path.join(exp_dir, "activations", f"patches_epoch_{epoch}.mat"), variable_names=[var]
+      )
       print(var)
       x = data[var]
       n = 18000 // (10 * x.shape[1])
@@ -37,23 +38,30 @@ if __name__ == "__main__":
       print(f"Labels: {l.shape}")
       x = x - x.mean(0)
       x = x / np.abs(x).max()
-      embedded = mds.fit_transform(x)
+      # embedded = mds.fit_transform(x)
+      d = cdist(x, x).mean()
       x = svd_reduction(x, n_components=None, threshold=0.98)
       print(f"After SVD: {x.shape}")
-      params = {"n_clusters": [None], "distance_threshold": [k for k in np.linspace(0.1, 20, 20)]}
+      params = {"n_clusters": [None], "distance_threshold": [k * d for k in np.linspace(0.2, 20, 20)]}
       clusters, p, scores = parameter_search(x, l, cluster.AgglomerativeClustering, params)
+      pprint(p)
       pprint(scores)
+      print()
+      out[var] = {"cluster_labels": clusters, "params": p, "scores": scores}
 
-      _, axs = plt.subplots(1, 2, tight_layout=True, figsize=(12, 6))
-      axs[0].scatter(embedded[:, 0], embedded[:, 1], c=l)
-      axs[0].grid(True)
-      axs[0].axis("equal")
-      axs[0].set_title(f"Original Labels (n = {len(set(l))})")
-      axs[1].scatter(embedded[:, 0], embedded[:, 1], c=clusters)
-      axs[1].grid(True)
-      axs[1].axis("equal")
-      axs[1].set_title(f"Cluster Labels (n = {len(set(clusters))})")
-      plt.suptitle(var)
-      plt.savefig(os.path.join(out_dir, var.replace(".", "_") + ".png"), bbox_inches="tight")
-      plt.clf()
-      plt.close()
+  with open(os.path.join(exp_dir, "clusters", f"patches_epoch_{epoch}.p"), "wb") as f:
+    p.dump(out, f, protocol=p.HIGHEST_PROTOCOL)
+
+      # _, axs = plt.subplots(1, 2, tight_layout=True, figsize=(12, 6))
+      # axs[0].scatter(embedded[:, 0], embedded[:, 1], c=l)
+      # axs[0].grid(True)
+      # axs[0].axis("equal")
+      # axs[0].set_title(f"Original Labels (n = {len(set(l))})")
+      # axs[1].scatter(embedded[:, 0], embedded[:, 1], c=clusters)
+      # axs[1].grid(True)
+      # axs[1].axis("equal")
+      # axs[1].set_title(f"Cluster Labels (n = {len(set(clusters))})")
+      # plt.suptitle(var)
+      # plt.savefig(os.path.join(out_dir, var.replace(".", "_") + ".png"), bbox_inches="tight")
+      # plt.clf()
+      # plt.close()
